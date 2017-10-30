876e69432cf91d6c2e22e59e4a3832598907807a
WiraXie <wiraxie.wira_xie@ymail.com>
2017-10-30 15:49:13 +0700

    6th

--- 1.js
+++ 1.js
@@ -1,90 +1,95 @@
-    var newObj = [
+var newObj = 
+[
     {
-        "id" : "1",
-        "name": "Mamat",
-        "wilayah": "Kebayoran Lama",
-        "telp": "021",
+        "ID" : "1",
+        "Nama": "Mamat",
+        "Wilayah": "Kebayoran Lama",
+        "Telp": "021",
         "Shift": "1"
     },
     {
-        "id" : "2",
-        "name": "Joni",
-        "wilayah": "Palmerah",
-        "telp": "089",
+        "ID" : "2",
+        "Nama": "Joni",
+        "Wilayah": "Palmerah",
+        "Telp": "089",
         "Shift": "2"
     },
     {
-        "id" : "3",
-        "name": "Saipul",
-        "wilayah": "Kebon Keruk",
-        "telp": "070",
+        "ID" : "3",
+        "Nama": "Saipul",
+        "Wilayah": "Kebon Keruk",
+        "Telp": "070",
         "Shift": "3"
     },
     {
-        "id" : "4",
-        "name": "Opi",
-        "wilayah": "Kemanggisan",
-        "telp": "001",
+        "ID" : "4",
+        "Nama": "Opi",
+        "Wilayah": "Kemanggisan",
+        "Telp": "001",
         "Shift": "2"
     },
     {
-        "id" : "5",
-        "name": "Udin",
-        "wilayah": "Tanjung Duren",
-        "telp": "054",
+        "ID" : "5",
+        "Nama": "Udin",
+        "Wilayah": "Tanjung Duren",
+        "Telp": "054",
         "Shift": "3"
         
     },
     {
-        "id" : "6",
-        "name": "Dudung",
-        "wilayah": "Grogol",
-        "telp": "099",
+        "ID" : "6",
+        "Nama": "Dudung",
+        "Wilayah": "Grogol",
+        "Telp": "099",
         "Shift": "1"
     },
     {
-        "id" : "7",
-        "name": "Si Baru",
-        "wilayah": "Kuningan",
-        "telp": "101",
+        "ID" : "7",
+        "Nama": "Si Baru",
+        "Wilayah": "Kuningan",
+        "Telp": "101",
         "Shift": "2"
-    },  
+    },
 ]
-window.onload = function CreateTableFromJSON() 
+
+window.onload = function tablejson() 
 {
-    var ubah;
-    var hapus;
-        // dapat value head. 
+        //dapat value head.//
         var col = [];
         for (var i = 0; i < newObj.length; i++) 
         {
             for (var key in newObj[i]) 
             {
-                if (col.indexOf(key) === -1) 
+                if (col.indexOf(key) === -1)  // EXTRACT VALUE FOR HTML HEADER. 
                 {
-                    col.push(key);
+                    col.push(key); 
+                    console.log(key);
                 }
-            }
+            }  
         }
+        console.log(newObj);
 
-        // bikin table.
+        //bikin table.//
         var table = document.createElement("table");
+        table.setAttribute("id","table");
+        table.setAttribute("class", "table table-bordered");
 
-        // bikin html row.
-        var tr = table.insertRow(-1);                   // row tabel.
+        //bikin html row.//
+        var tr = table.insertRow(-1);                   //row tabel.
 
         for (var i = 0; i < col.length; i++) 
         {
-            var th = document.createElement("th");      // header table.
+            var th = document.createElement("th");      //header table.
             th.innerHTML = col[i];
             tr.appendChild(th);
         }
 
-        // json masuk row.
+        //json masuk row.//
         for (var i = 0; i < newObj.length; i++) 
         {
             tr = table.insertRow(-1);
-
+            tr.setAttribute("id", "row"+i);
+            console.log(tr);
             // tabCell = Element.outerHTML = "<button>edit</button>";
             // tabCell = Element.outerHTML = "<button>remove</button>";
             
@@ -92,33 +97,27 @@ window.onload = function CreateTableFromJSON()
             for (var j = 0; j < col.length; j++) 
             {
                 var tabCell = tr.insertCell(-1);
-                tabCell.innerHTML = newObj[i][col[j]]
-
+                tabCell.innerHTML = newObj[i][col[j]];
             }
+            //cell loop end
+
+            //tombol ubah
             ubah = tr.insertCell(-1);
-            ubah.outerHTML = "<button>EDIT</button>";
+            ubah.innerHTML = "<button onclick='klik()'>Ubah</button>";
+            
+              function klik()
+              {
+                document.getElementById("formDiv").style.display = "block";
+              }
 
+            //tombol hapus
             hapus = tr.insertCell(-1);
-            hapus.outerHTML = "<button>DELETE</button>"; 
+            hapus.innerHTML = "<button>Hapus</button>"; 
+            
         }
-        //ikin button dari panjang json
-        // for (var i =0; i<newObj.length; i++)
-        // { 
-        //     ubah = tr.insertCell(-1);
-        //     ubah.outerHTML = "<button>EDIT</button>";  
-        // }
-        // console.log(ubah);
-
-        // for(var j=0; j<newObj.length; j++)
-        // {
-        //     hapus = tr.insertCell(-1);
-        //     hapus.outerHTML = "<button>DELETE</button>";  
-        // }
-        // console.log(hapus);
 
         // container.
         var divContainer = document.getElementById("showData");
         divContainer.innerHTML = "";
         divContainer.appendChild(table);
- }
-
+}
